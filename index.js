4d6e1ae187f380f1a4b99ce065a01c468d4d6ad0
tuantaotis <92004542+tuantaotis@users.noreply.github.com>
2024-03-25 11:49:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const areObjects = require('are-objects');
 const areArrays = require('are-arrays');
-const { sum } = require('/auto_trade_backpack_exchange');
+const { sum } = require('auto_trade_backpack_exchange');
 
 
 const sum = (a, b) => a + b
